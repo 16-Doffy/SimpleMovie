10757bc34423de48acaea9390d57e4bbdb00fb91
Nhut Duy <ngophamnhutduy050302@gmail.com>
2025-03-24 19:03:53 +0700

    finish detailmovie page

--- src/pages/DetailMovie.jsx
+++ src/pages/DetailMovie.jsx
@@ -1,6 +1,9 @@
 import { useParams } from "react-router-dom";
 import useSWR from "swr";
 import { fetcher } from "../config/config";
+import { SwiperSlide } from "swiper/react";
+import Swiper from "swiper";
+import MovieCard from "../Components/Movie/MovieCard";
 
 const DetailMovie = () => {
   const { moviesId } = useParams(); // Lấy moviesId từ useParams()
@@ -11,7 +14,7 @@ const DetailMovie = () => {
 
   console.log("data", data);
   if (!data) return null;
-  const { title, backdrop_path, genres, overview, character } = data;
+  const { title, backdrop_path, genres, overview } = data;
 
   return (
     <div className="py-10">
@@ -59,8 +62,12 @@ const DetailMovie = () => {
       >
         {overview}
       </p>
+      <div className="w-20 flex m-auto text-center border text-white"></div>
       <MovieCredit></MovieCredit>
+      <div className="w-20 flex m-auto text-center border text-white mt-10"></div>
       <MovieVideos></MovieVideos>
+      <div className="w-20 flex m-auto text-center border text-white mt-10"></div>
+      <MovieSilimar></MovieSilimar>
     </div>
   );
 };
@@ -85,9 +92,9 @@ function MovieCredit() {
             <img
               src={`https://image.tmdb.org/t/p/original${item.profile_path}`}
               alt=""
-              className="w-full h-[350px] object-cover rounded-lg mb-3"
+              className="w-full h-full object-cover rounded-lg mb-3"
             />
-            <h3 className="text-center text-2xl">{item.name}</h3>
+            <h3 className="text-center text-4xl">{item.name}</h3>
           </div>
         ))}
       </div>
@@ -102,8 +109,61 @@ function MovieVideos() {
     `https://api.themoviedb.org/3/movie/${moviesId}/videos?api_key=733d08f3b55d5c3b516692a4f30a1ff7`,
     fetcher
   );
+
   console.log("video", data);
 
-  return <div></div>;
+  if (!data || !data.results || data.results.length === 0)
+    return <p>Không có video nào</p>;
+
+  return (
+    <div className="py-10">
+      <div className="flex flex-col gap-10">
+        {data.results.slice(0,1).map((item) => (
+          <div key={item.id}>
+            <h3 className="text-3xl text-white mb-2  bg-blue-800 font-bold w-100 h-15 p-3 text-center">
+              {item.name}
+            </h3>
+            <div className="w-full aspect-video">
+              <iframe
+                width="560"
+                height="315"
+                src={`https://www.youtube.com/embed/${item.key}`}
+                frameBorder="0"
+                allowFullScreen
+                className="w-full h-full object-fill"
+              ></iframe>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
+// https://api.themoviedb.org/3/movie/{movie_id}/similar
+
+function MovieSilimar () {
+  const { data } = useSWR(
+    `https://api.themoviedb.org/3/movie/popular?api_key=733d08f3b55d5c3b516692a4f30a1ff7`,
+    fetcher
+  );
+  const movies = data?.results || [];
+  console.log("moviespage", movies);
+  return (
+    <div className="py-10">
+   <h3 className="text-3xl text-white mb-2  bg-blue-800 font-bold w-100 h-15 p-3 text-center ">Similar Movie</h3>
+      <div className="flex  flex-row gap-10 m-auto">
+        {movies.length > 10 &&
+          movies.slice(0,4).map((item) => (
+            <MovieCard key={item.id} item={item}></MovieCard>
+          ))}
+          <h1><img src="https://cdn-icons-png.flaticon.com/256/10310/10310074.png" alt=""
+            className="py-50 px-24"
+          /> <p className="text-6xl border-2 w-full h-35 text-center p-1 bg-blue-700 rounded-2xl">
+          Back To HomePage
+          </p></h1>
+          
+      </div>
+    </div>
+  );
+};
 export default DetailMovie;
